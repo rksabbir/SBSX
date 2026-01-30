13205b61d0f02eee78f3af13d9e6cf1981850b1c
RK SABBIR BHAI <sabbirpaypl@outlook.com>
2026-01-30 09:31:08 +0600

    Update test.js

--- functions/test.js
+++ functions/test.js
@@ -1,3 +1,24 @@
-export function onRequest() {
-  return new Response("Functions OK");
+export async function onRequest() {
+  try {
+    // hidden original JS
+    const hiddenURL =
+      "https://sbsx.pages.dev/SbmxIntegratedjettheme/js@0.5.9/main.js";
+
+    // Fetch original JS
+    const res = await fetch(hiddenURL);
+    const code = await res.text();
+
+    // Serve JS to browser via /app.js
+    return new Response(code, {
+      headers: {
+        "Content-Type": "application/javascript; charset=utf-8",
+        "Cache-Control": "public, max-age=86400"
+      }
+    });
+  } catch (err) {
+    return new Response(
+      'console.error("Failed to load SBMX JS");',
+      { headers: { "Content-Type": "application/javascript" } }
+    );
+  }
 }
