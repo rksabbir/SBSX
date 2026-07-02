d5190e6ecf2466f6f648cdf69e177c7cc49e13ff
RK SABBIR BHAI <sabbirpaypl@outlook.com>
2026-07-02 07:00:03 +0600

    Set up Express server to respond on root route

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Bot is running!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`🌐 Web server running on port ${PORT}`);
+});
+
 const { 
     Client, 
     GatewayIntentBits, 
